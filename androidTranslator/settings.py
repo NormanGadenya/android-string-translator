a1f208475eb3654a98875f004bdfa2a05d1d08db
Ngadenya <ngadenya@gmail.com>
2022-12-22 13:57:30 +0300

    Updated allowed hosts to all

--- androidTranslator/settings.py
+++ androidTranslator/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(env('DEBUG_MODE'))
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
